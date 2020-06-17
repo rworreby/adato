fdfe8ae6b237722fd9010c11024f24c136b7fdd0
Robin Worreby <rworreby@student.ethz.ch>
2020-06-17 02:48:29 +0200

    Fix formatting and remove unused imports

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -6,7 +6,6 @@ from hypothesis import given, example, assume
 from hypothesis import strategies as st
 from hypothesis.extra.pandas import data_frames, column
 
-import string
 import re
 import pandas as pd
 
@@ -37,8 +36,6 @@ from preprocessing.data_cleaning import clean_hash39
 #         st.from_regex(regex).example()
 #     )
 
-
-
 # for i in range(10):
 #     print(data_frames([column('description',
 #                         elements=st.text(
@@ -53,7 +50,6 @@ from preprocessing.data_cleaning import clean_hash39
 #         st.from_regex(regex).example()
 #     )
 
-
 # @given(data_frames([column('title',
 #                     elements=st.text(
 #                         alphabet=string.ascii_letters+r' #;\''
@@ -63,18 +59,24 @@ from preprocessing.data_cleaning import clean_hash39
 
 
 regex = re.compile(r'.* #39;.*', re.ASCII)
-@given(input_df=data_frames([column('title',
-                    elements=st.from_regex(regex),
-                    unique=True)
-                    ]))
-@example(input_df=pd.DataFrame({'title':
-                    ['Robin #39;s forever super string test.', 'String 1']}
-                    ))
+
+
+@given(input_df=data_frames(
+    [column(
+        'title',
+        elements=st.from_regex(regex),
+        unique=True
+    )]
+))
+@example(input_df=pd.DataFrame(
+    {'title':
+        ['Robin #39;s forever super string test.', 'String 1']}
+))
 def test_clean_hash39(input_df):
     assume(input_df.shape[0] > 0)
     cleaned_df = clean_hash39(input_df)
 
-    #cleaned_df.loc[0] = 'This is Robin #39;s wrong example.'
+    # cleaned_df.loc[0] = 'This is Robin #39;s wrong example.'
 
     for _, row in cleaned_df.iterrows():
         assert ' #39;' not in row.title
