94c34e8aad7f7a6a2ed202db29c1039b3f485d1a
Robin Worreby <rworreby@student.ethz.ch>
2020-06-17 02:31:44 +0200

    add tests preprocessing pipeline

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,6 +2,75 @@
 Unit tests for the preprocessing pipeline
 """
 
+from hypothesis import given, example, assume
+from hypothesis import strategies as st
+from hypothesis.extra.pandas import data_frames, column
 
-def test_dummy():
-    assert 1 == 1
+import string
+import re
+
+from preprocessing.data_cleaning import clean_hash39
+
+
+# for i in range(10):
+#     print(
+#         st.text(
+#             st.sampled_from(
+#                 r'\\_/?:^\'"[a-zA-Z0-9]',
+#             ),
+#             min_size=5,
+#         ).example()
+#     )
+#
+# for i in range(10):
+#     print(
+#         st.text(
+#             alphabet=string.ascii_letters+r' /\\?-^\'"_:',
+#             min_size=5,
+#         ).example()
+#     )
+#
+# regex = re.compile(r'(\w+\s){5}', re.ASCII)
+# for i in range(10):
+#     print(
+#         st.from_regex(regex).example()
+#     )
+
+
+
+# for i in range(10):
+#     print(data_frames([column('description',
+#                         elements=st.text(
+#                             alphabet=string.ascii_letters+r' #;/\\?-^\'"_:'
+#                         ),
+#                         unique=True)
+#                 ]).example())
+
+# regex = re.compile(r'.* #39;.*', re.ASCII)
+# for i in range(10):
+#     print(
+#         st.from_regex(regex).example()
+#     )
+
+
+# @given(data_frames([column('title',
+#                     elements=st.text(
+#                         alphabet=string.ascii_letters+r' #;\''
+#                     ),
+#                     unique=True)
+#                     ]))
+
+
+regex = re.compile(r'.* #39;.*', re.ASCII)
+@given(data_frames([column('title',
+                    elements=st.from_regex(regex),
+                    unique=True)
+                    ]))
+def test_clean_hash39(input_df):
+    assume(input_df.shape[0] > 0)
+    cleaned_df = clean_hash39(input_df)
+
+    #cleaned_df.loc[0] = 'This is Robin #39;s wrong example.'
+
+    for _, row in cleaned_df.iterrows():
+        assert ' #39;' not in row.title
