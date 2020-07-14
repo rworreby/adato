100724f015d2db305cedaa91c19f8bc4625f29c9
Robin Worreby <rworreby@student.ethz.ch>
2020-07-14 17:35:20 +0200

    Add __init__ files

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .preprocessing.data_cleaning import *
+__all__ = ['clean_hash36',
+		   'clean_hash39',
+		   'clean_multiple_whitespace',
+		   'clean_trailing_leading',
+		   'clean_title_remarks',
+		   'clean_description_remarks',
+		   ]
---
+++ preprocessing/__init__.py
@@ -0,0 +1,9 @@
+from .data_cleaning import clean_hashes, clean_trailing_leading
+from .data_cleaning import clean_title_remarks, clean_description_remarks
+
+__all__ = [
+    'clean_hashes',
+    'clean_trailing_leading',
+    'clean_title_remarks',
+    'clean_description_remarks',
+    ]
