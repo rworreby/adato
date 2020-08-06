9fc41393d4fbb2ce92b9e48181e66f639ef07ec8
Robin Worreby <rworreby@student.ethz.ch>
2020-08-06 10:58:39 +0200

    Update imports in adato __init__ file

--- __init__.py
+++ __init__.py
@@ -1,11 +1,7 @@
-from .preprocessing.data_cleaning import clean_hashes
-from .preprocessing.data_cleaning import clean_trailing_leading
-from .preprocessing.data_cleaning import clean_title_remarks
-from .preprocessing.data_cleaning import clean_description_remarks
+from adato import preprocessing
+from adato import model
 
 __all__ = [
-    'clean_hashes',
-    'clean_trailing_leading',
-    'clean_title_remarks',
-    'clean_description_remarks',
+    'preprocessing',
+    'model',
     ]
