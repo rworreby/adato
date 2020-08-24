fa5c37cb7ce877bda98f7360e07fa4d7560b9db0
Robin Worreby <robin@worreby.ch>
2020-08-24 14:51:47 +0200

    Add labeling page routing in app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+
+labeling_counter = 0
+
+path = './adato/data/'
+train_df = pd.read_csv(
+    path + 'cleaned_train.csv',
+    header=0,
+    names=['classid', 'title', 'description'],
+)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('label', number=labeling_counter))
+
+
+@app.route('/label/')
+@app.route('/label/<number>')
+def label(number=0):
+    labeling_counter = int(number)
+    return render_template('label.html',
+                           data=train_df.iloc[labeling_counter]['description'],
+                           data_counter=labeling_counter,
+                           )
 
 
 @app.route('/about')
